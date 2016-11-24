e12c95a058b687c6292fd1a741209a7028c8e702
CY Lim <cylim226@gmail.com>
2016-11-24 16:03:49 +1100

    Fix: Able to get Date and timestamp

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var moment = require('moment');
 
-app.get('/', function(req, res){
-    res.send("Hello world!")
-})
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
+app.get('/:query', function(req, res) {
+    var date = req.params.query;
+    var unix = null;
+    var natural = null;
+    
+    // Check for initial unix time
+    if (+date >= 0) {
+        unix = +date;
+        natural = toTimestamp(unix);
+    } 
+    
+    // Check for initial natural time
+    if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+        unix = +toTimestamp(date);
+        natural = toDate(unix);
+    }
+    
+    var dateObj = { "unix": unix, "natural": natural };
+    res.json(dateObj);
+});
+
+function toTimestamp(date) {
+    // Conver from natural date to unix timestamp
+    return moment(date, "MMMM D, YYYY").format("X");
+}
+
+function toDate(unix) {
+    // Convert unix timestamp to natural date
+    return moment.unix(unix).format("MMMM D, YYYY");
+}
 
 app.listen(8080, function(){
-    console.log("Running at 8080")
-})
+    console.log("Running at 8080");
+});
